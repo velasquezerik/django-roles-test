949781997bcad86b5ee526fb135a9477d73a776e
Erik Velasquez <erikvelasquez.25@gmail.com>
2016-11-08 18:06:05 -0400

    New Socket server definition

--- Sockets/Java/TCPServer.java
+++ Sockets/Java/TCPServer.java
@@ -21,9 +21,54 @@
 import java.io.*;
 import java.net.*;
 
+/*public class KKMultiServer {
+    public static void main(String[] args) throws IOException {
+
+    if (args.length != 1) {
+        System.err.println("Usage: java KKMultiServer <port number>");
+        System.exit(1);
+    }
+
+        int portNumber = Integer.parseInt(args[0]);
+        boolean listening = true;
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            while (listening) {
+	            new KKMultiServerThread(serverSocket.accept()).start();
+	        }
+	    } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}*/
+
 class TCPServer
 {
-   public static void main(String argv[]) throws Exception
+  /*public static void main(String[] args) throws IOException {
+
+    if (args.length != 1)
+    {
+      System.err.println("Usage: java TCPServer <port number>");
+      System.exit(1);
+    }
+    //Get the port number
+    int portNumber = Integer.parseInt(args[0]);
+
+    String fromclient;
+    String toclient;
+    try{
+
+      //init de the server on port number
+      ServerSocket Server = new ServerSocket (portNumber);
+      System.out.println ("TCPServer Waiting for client on port " + portNumber);
+    }
+    catch (IOException e) {
+        System.out.println(e);
+    }
+
+  }*/
+   public static void main(String argv[]) throws IOException
       {
          String fromclient;
          String toclient;
